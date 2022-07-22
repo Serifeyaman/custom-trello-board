200ac1b212593e969dbfbc3592cb714ad45f0cb5
serifeyaman <serifeyaman@ozbelsan.com.tr>
2022-07-22 14:21:49 +0300

    liste ekleme component değişikliğ

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import MyAddCardLink from 'components/MyAddCardLink';
 import MyLaneForm from 'components/MyLaneForm';
 import MyLaneHeader from 'components/MyLaneHeader';
 import React from 'react'
+import { Plus } from 'react-feather';
 import { useDispatch, useSelector } from 'react-redux';
 import Board from 'react-trello'
+import { Button } from 'reactstrap';
 import { getLanes } from 'redux/lane/action';
 
 const App = () => {
@@ -24,10 +26,12 @@ const App = () => {
       components={{
         // GlobalStyle: MyGlobalStyle, 
         // Card: MyCard,
+        NewLaneSection: (props) => <Button onClick={() => props.onClick()} color='secondary' size='md' style={{ fontSize: 14, backgroundColor:'#306073', border:'none', margin:5 }}> <Plus size={15} /> Liste Ekle</Button>,
         NewLaneForm: (props) => MyLaneForm({ ...props, lanesArray: lanesArray }),
-        LaneHeader: (props) => MyLaneHeader({ ...props, lanesArray: lanesArray  }),
+        LaneHeader: (props) => MyLaneHeader({ ...props, lanesArray: lanesArray }),
+
         AddCardLink: (props) => MyAddCardLink({ ...props }),
-        NewCardForm: (props) => MyAddCardForm({ ...props, lanesArray: lanesArray })
+        NewCardForm: (props) => MyAddCardForm({ ...props, lanesArray: lanesArray }),
       }}
       data={{ lanes: lanesArray }}
       laneStyle={{ backgroundColor: 'rgba(176, 126, 204, 0.4)', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }} // büyük kart css
